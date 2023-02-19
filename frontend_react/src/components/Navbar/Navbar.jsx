249910d7822f63b7cf08fb550e223f16efdc9a1e
Shiba <66871507+shiba196@users.noreply.github.com>
2023-02-20 00:27:47 +0530

    Some changes

---
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import './Navbar.scss';
+import { images } from '../../constants';
+
+const Navbar = () => {
+  return (
+    <nav className="app_navbar">
+    <div>
+      <img src={images.logo} alt="logo" />
+    </div>
+    <ul>
+      {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+        <li key={'link-${item}'}>
+          <div />
+          <a href={'#${item}'}>{item}</a>
+        </li>
+      ) )}
+    </ul>
+    </nav>
+  )
+}
+
+export default Navbar
